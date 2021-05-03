08010b1958c80a05e50897b70d5320838cf393f9
Lee-Gain <s3878170@rmit.edu.vn>
2021-05-03 12:29:48 +0700

    Fixed minor problems in checkstatus.js.
    I fixed minor problems in checkstatus.js.

--- js/checkstatus.js
+++ js/checkstatus.js
@@ -5,7 +5,7 @@ window.addEventListener('load', changeMyPagePath);
 function changeMyPagePath(){
     const myPagePath = document.getElementById("account");
     const newPath = "mypage.html"
-    if(localStorage.getItem("userCurrentId").length > 0 ){
+    if(localStorage.getItem("userCurrentId") !== null){
         myPagePath.setAttribute("href",newPath);
     }
 }
