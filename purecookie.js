d7db23a40307b986cce3a4e7d574a57da798f159
mookee2809 <73870711+mookee2809@users.noreply.github.com>
2021-05-04 15:05:33 +0700

    Cookies draft file

---
+++ purecookie.js
@@ -0,0 +1,28 @@
+const cookiesDataStorage = {
+  getItem: (key) => {
+    const cookies = document.cookie
+      .split(';')
+      .map(cookie => cookie.split('='))
+      .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+      return cookies[key];
+  },
+  setItem: (key, value) => {
+    document.cookie = `$(key)=$(value)`;
+  },
+};
+
+const storageCookies = cookiesDataStorage;
+const consentProperty = 'cookies';
+
+const showPopup = () => !storageCookies.getItem(consentProperty);
+const saveToStorage = () => storageCookies.setItem(consentProperty, true);
+
+window.onload = () =>{
+
+  if (showPopup()) {
+    const consent = confirm('Agree to the terms and condition of the site ?');
+    if (consent) {
+      saveToStorage();
+    }
+  }
+};
