b1157ad02b9b3fcc276fc8433911b030e04a7079
mookee2809 <73870711+mookee2809@users.noreply.github.com>
2021-05-07 15:34:13 +0700

    Updated better cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,16 +1,21 @@
-const cookieBox = document.querySelector(".popup_cookies"),
-acceptBtn = cookieBox.querySelector(".buttons button");
+'use strict';
+const cookieBox = document.querySelector(".popup_cookies");
+const acceptBtn = cookieBox.querySelector(".cookies_buttons button");
 
 //Expire the cookies after 1 month
-acceptBtn.onclick = ()=>{
-document.cookie = "YourCookies; max-age="+60*60*24*30;
-if(document.cookie){
-    cookieBox.classList.add("hide"); 
-    //if you already accpet, then hide cookie//
+
+acceptBtn.addEventListener('click', cookies);
+
+function cookies(){
+    document.cookie = "YourCookies; max-age=" + (60 * 60 * 24 * 30);
+    if(document.cookie){
+        cookieBox.classList.add("hide"); 
+        //if you already accpet, then hide cookie//
+    }
+    else{
+        alert("Cookie error!");
+    }
 }
-else{
-    alert("Cookie error!");
-}
-}
+
 let checkCookie = document.cookie.indexOf("YourCookies");
-checkCookie != -1 ? cookieBox.classList.add("hide"): cookieBox.classList.remove("hide");
+checkCookie !== -1 ? cookieBox.classList.add("hide"): cookieBox.classList.remove("hide");
